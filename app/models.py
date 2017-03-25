01cc45d0806eb8a0f744b53f63377335bd5c1c0e
Leo Karabeg <lkarabeg@gmail.com>
2017-03-25 16:01:44 +0100

    Update models

--- app/models.py
+++ app/models.py
@@ -1,23 +1,30 @@
 
-
-class Customer(object):
-    def __init__(self, name, orgnr):
-        self.name = name
-        self.orgnr = orgnr
-    def update(self, **kwargs):
+class Base(object):
+    def __init__(self, **kwargs):
         if kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
+    def update(self, **kwargs): #TODO: Remember ID
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
     def __repr__(self):
-        return "<Customer object; name={}, orgnr={}>".format(self.name, self.orgnr)
+        repr_fields = getattr(self, '__repr_fields__', ['id'])
 
+        repr_values = ', '.join(
+                '{}:{}'.format(
+                repr_field,
+                getattr(self, repr_field, None)
+            )
+            for repr_field in repr_fields
+        )
 
-def printstuff(**kwargs):
-    print("kwargs: ",kwargs)
-    print("*kwargs: ",*kwargs)
-    print("**kwargs: ",**kwargs)
-    for key, value in kwargs.items():
-        print(key, value)
+        return "<class:{} {}>".format(
+            self.__class__.__name__,
+            repr_values
+        )
 
-stuff = {'foo':'bar'}
+class Customer(Base):
+    __repr_fields__ = ['name', 'orgnr']
+    pass
+
